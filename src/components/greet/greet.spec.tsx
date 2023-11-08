1f2479bc3ff829f7aa6acb51d634c865ee75878c
rashdanmd <rashdan.developer@gmail.com>
2023-11-08 21:01:08 +0000

    first commit

---
+++ src/components/greet/greet.spec.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import { Greet } from "./greet";
+
+// test("Greet renders correctly", () => {
+//   render(<Greet />);
+//   const text = screen.getByText(/Hello/i);
+//   expect(text).toBeInTheDocument();
+// });
+
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("hello");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test("renders correctly with user's name", () => {
+    render(<Greet name="Moto" />);
+    const textElement = screen.getByText("hello Moto");
+    expect(textElement).toBeInTheDocument();
+  });
+});
---
+++ src/components/greet/greet.tsx
@@ -0,0 +1,19 @@
+// export const Greet = () => {
+//   return <div>hello</div>;
+// };
+
+// TDD approach - brief for RED-GREEN approach
+// *Greet should the text hello
+// *It should render hello followed by the name
+
+type GreetProps = {
+  name?: string;
+};
+
+// export const Greet = (props: GreetProps) => {
+//   return <div>hello {props.name}</div>;
+// };
+
+export const Greet = ({ name }: GreetProps) => {
+  return <div>hello {name}</div>;
+};
