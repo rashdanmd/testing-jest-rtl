43b726bd63d1c4c0dc4db039f0ae0a8676689bf6
rashdanmd <rashdan.developer@gmail.com>
2023-11-11 21:11:09 +0000

    getBy tests

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -1,8 +1,77 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Application from "./application";
 
 describe("Application", () => {
   test("renders correctly", () => {
     render(<Application />);
+    const pageHeading = screen.getByRole("heading", {
+      //   name: "Job Application Form",
+      level: 1,
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    const sectionHeadingElement = screen.getByRole("heading", {
+      name: "Section 1",
+      level: 2,
+    });
+    expect(sectionHeadingElement).toBeInTheDocument();
+
+    const pElement = screen.getByText("All fields are mandatory");
+    expect(pElement).toBeInTheDocument();
+
+    const nameElem = screen.getByRole("textbox", { name: "Name" });
+    expect(nameElem).toBeInTheDocument();
+
+    /***** getBy - LabelText ******/
+    const nameElement2 = screen.getByLabelText("Name");
+    expect(nameElement2).toBeInTheDocument();
+
+    const agreeLabel = screen.getByLabelText(
+      "I agree to the terms and conditions"
+    );
+    expect(agreeLabel).toBeInTheDocument();
+
+    const bioElem2 = screen.getByLabelText("Bio");
+    expect(bioElem2).toBeInTheDocument();
+
+    // const name2 = screen.getByLabelText("Name", { selector: "name" });
+    // expect(name2).toBeInTheDocument();
+    /***** getBy - LabelText ******/
+
+    const bioElem = screen.getByRole("textbox", { name: "Bio" });
+    expect(bioElem).toBeInTheDocument();
+
+    const jobLocationElement = screen.getByRole("combobox");
+    expect(jobLocationElement).toBeInTheDocument();
+
+    const termsConditionsElement = screen.getByRole("checkbox");
+    expect(termsConditionsElement).toBeInTheDocument();
+
+    const submitElement = screen.getByRole("button");
+    expect(submitElement).toBeInTheDocument();
+
+    const namePlaceholder = screen.getByPlaceholderText(/Full Name/i);
+    expect(namePlaceholder).toBeInTheDocument();
+
+    /*****  getBy DisplayValue  ****/
+    const nameDisplayValue = screen.getByDisplayValue("John Doe");
+    expect(nameDisplayValue).toBeInTheDocument();
+    /*****  getBy DisplayValue  ****/
+
+    /*****  getBy AltText  ****/
+    const imgAltText = screen.getByAltText("brown shoes with laces");
+    expect(imgAltText).toBeInTheDocument();
+    /*****  getBy AltText  ****/
+
+    /*****  getBy Title  ****/
+    const closeElem = screen.getByTitle("close");
+    expect(closeElem).toBeInTheDocument();
+    /*****  getBy Title  ****/
+
+    /*****  getBy TestId ****/
+    const customElem = screen.getByTestId("custom-element");
+    expect(customElem).toBeInTheDocument();
+
+    /*****  getBy TestId  ****/
   });
 });
--- src/components/application/application.tsx
+++ src/components/application/application.tsx
@@ -1,29 +1,49 @@
 const Application = () => {
   return (
-    <form>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name"></input>
-      </div>
-      <div>
-        <label htmlFor="job-location">Job location</label>
-        <select>
-          <option value="">Select a country</option>
-          <option value="US">United States</option>
-          <option value="GB">United Kingdom</option>
-          <option value="CA">Canada</option>
-          <option value="IN">India</option>
-          <option value="AU">Australia</option>
-        </select>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" id="terms" />I agree to the terms and
-          conditions
-        </label>
-        <button>Submit</button>
-      </div>
-    </form>
+    <>
+      <h1>Job Application Form</h1>
+      <h2>Section 1</h2>
+      <p>All fields are mandatory</p>
+      <span title="close">X</span>
+      <img
+        src="https://randomwordgenerator.com/img/picture-generator/57e5d34a4c50a414f1dc8460962e33791c3ad6e04e507440762e7adc9548cd_640.jpg"
+        alt="brown shoes with laces"
+      />
+      <div data-testid="custom-element">CUSTOM HTML element</div>
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Full name"
+            defaultValue="John Doe"
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="bio">Bio</label>
+          <textarea id="bio"></textarea>
+        </div>
+        <div>
+          <label htmlFor="job-location">Job location</label>
+          <select>
+            <option value="">Select a country</option>
+            <option value="US">United States</option>
+            <option value="GB">United Kingdom</option>
+            <option value="CA">Canada</option>
+            <option value="IN">India</option>
+            <option value="AU">Australia</option>
+          </select>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" id="terms" />I agree to the terms and
+            conditions
+          </label>
+          <button>Submit</button>
+        </div>
+      </form>
+    </>
   );
 };
 
